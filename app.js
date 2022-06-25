488e0be85cb798cbf5132ba46ed38214086ba3e5
bajenoB <jonjill1488@gmail.com>
2022-06-25 16:12:28 +0300

    231
    1231

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const config=require('config')
 const express=require('express')
 const mongoose=require('mongoose')
+const expresshandlebars = require('express-handlebars')
 const app=express()
 
 const PORT=config.get('port')
+
+const homeRoute=require('./routes/home')
+
+const hbs=expresshandlebars.create({
+    defaultLayout:'main',
+    extname:'hbs'
+})
+
+app.engine('hbs',hbs.engine)
+app.set('view egine','hbs')
+app.set('views','views')
+
+app.use(express.urlencoded({
+    extended:true
+}))
+
+app.use(homeRoute)
 
 async function start(){
 try {
