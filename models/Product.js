064c56fcd0adffe8c9269fb3c31a175222e92c49
bajenoB <jonjill1488@gmail.com>
2022-06-25 23:30:51 +0300

    хуйня
    сука как сделать ебучую менюшку сбоку

---
+++ models/Product.js
@@ -0,0 +1,15 @@
+const{
+    Schema,model
+
+}=require('mongoose')
+
+const schema=new Schema({
+    Name: {type:String,required:true, unique: false},
+    Price:{type:Number,required:true, unique: false},
+    Manufakturer:{type:String,required:true, unique: false},
+    Image:{type:String,required:true, unique: false},
+    Country:{type:String,required:true, unique: false},
+    Rating:{type:Number,required:true, unique: false}
+    
+})
+module.exports=model('Product',schema)
--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,13 @@
 const {Router} = require('express')
 const router=Router()
+const Product=require('../models/Product')
 router.get('/',async(req,res)=>
 {
+    const products=await Product.find({}).lean()
+
     res.render('index',{
-        title: 'toliksite'
+        title: 'toliksite',
+        products
     })
 })
 router.get('/admin',async(req,res)=>
@@ -12,4 +16,21 @@ router.get('/admin',async(req,res)=>
         title: 'adminsite'
     })
 })
+
+router.post('/createProd',async(req,res)=>
+{
+    const product= new Product({
+        Name: req.body.Name,
+        Price: req.body.Price,
+        Image: req.body.Image,
+        Manufakturer: req.body.Manufakturer,
+        Country: req.body.Country,
+        Rating: req.body.Rating
+        
+    })
+    await product.save()
+    res.redirect('/')
+})
+
+
 module.exports = router
